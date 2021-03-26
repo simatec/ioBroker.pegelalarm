12ecf3f3da42dece783808627582ba37944e9580
simatec <nais@gmx.net>
2021-03-26 13:07:41 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,65 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+  "country": {
+    "en": "country",
+    "de": "Land",
+    "ru": "страна",
+    "pt": "país",
+    "nl": "land",
+    "fr": "pays",
+    "it": "nazione",
+    "es": "país",
+    "pl": "kraj",
+    "zh-cn": "国家"
+  },
+  "region": {
+    "en": "region",
+    "de": "Region",
+    "ru": "область",
+    "pt": "região",
+    "nl": "regio",
+    "fr": "Région",
+    "it": "regione",
+    "es": "región",
+    "pl": "region",
+    "zh-cn": "地区"
+  },
+  "water": {
+    "en": "stretch of water",
+    "de": "Gewässer",
+    "ru": "полоса воды",
+    "pt": "extensão de água",
+    "nl": "stuk water",
+    "fr": "plan d'eau",
+    "it": "specchio d'acqua",
+    "es": "estanque",
+    "pl": "odcinek wody",
+    "zh-cn": "绵延的水"
+  },
+  "stationname": {
+    "en": "stationname",
+    "de": "Stationsname",
+    "ru": "Название станции",
+    "pt": "Nome da estação",
+    "nl": "station naam",
+    "fr": "Nom de la station",
+    "it": "Nome stazione",
+    "es": "Nombre de estación",
+    "pl": "Nazwa stacji",
+    "zh-cn": "站名"
+  },
+  "response": {
+    "en": "response",
+    "de": "Antwort",
+    "ru": "ответ",
+    "pt": "resposta",
+    "nl": "reactie",
+    "fr": "réponse",
+    "it": "risposta",
+    "es": "respuesta",
+    "pl": "odpowiedź",
+    "zh-cn": "响应"
+  }
+};
